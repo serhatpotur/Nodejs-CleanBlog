e93d88488fdc36ab56eea17528bd56032d1efc7d
Serhat <serhatpotur1@gmail.com>
2022-06-09 22:48:14 +0300

    View Engine

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require("express");
+const ejs = require('ejs');
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  const blog = {
-    id: 1,
-    title: "Blog title",
-    description: "Blog description",
-  };
-  res.send(blog);
+app.use(express.static("public"));
+
+app.set('view engine','ejs');
+
+app.get('/', (req, res) => {
+  res.render('index')
 });
+app.get('/about', (req, res) => {
+  res.render('about')
+});
+app.get('/post', (req, res) => {
+  res.render('post')
+});
+app.get('/add', (req, res) => {
+  res.render('add')
+});
+
 
 app.listen(port, () => {
   console.log(`Sunucu ${port} portuyla çalışmaya başladı`);
