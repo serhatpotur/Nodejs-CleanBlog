ca295ae6952daa6684d85f1f71824ab00d1bed15
Serhat <serhatpotur1@gmail.com>
2022-06-11 16:53:01 +0300

    Add Model and Add New Blog

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
 const express = require("express");
-const ejs = require('ejs');
+const mongoose = require("mongoose");
+const Blog = require("./models/Blog");
+const ejs = require("ejs");
 const app = express();
 const port = 3000;
 
+//Middleware
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
-app.set('view engine','ejs');
+// db Connect
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cleanblog-test-db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.get('/', (req, res) => {
-  res.render('index')
-});
-app.get('/about', (req, res) => {
-  res.render('about')
-});
-app.get('/post', (req, res) => {
-  res.render('post')
-});
-app.get('/add', (req, res) => {
-  res.render('add')
-});
+app.set("view engine", "ejs");
 
+app.get("/", async (req, res) => {
+  const blogs= await Blog.find({});
+  res.render("index",{
+    blogs
+  });
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/post", (req, res) => {
+  res.render("post");
+});
+app.get("/add", (req, res) => {
+  res.render("add");
+});
+app.post("/blogs", async (req, res) => {
+  await Blog.create(req.body)
+  //console.log(req.body);
+  res.redirect("/");
+});
 
 app.listen(port, () => {
   console.log(`Sunucu ${port} portuyla çalışmaya başladı`);
---
+++ models/Blog.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema({
+  title: String,
+  detail: String,
+  image: String,
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+module.exports = Blog;
